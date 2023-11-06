41c62c51f4f9684c1904bc18279c119e8ac534c1
Danrley Ferraz <110141527+Danrley-Ferraz@users.noreply.github.com>
2023-11-06 14:25:38 -0300

    Aulas

---
+++ java_anatomia_classes/src/Aula02.java
@@ -0,0 +1,17 @@
+public class Aula02 {
+    
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        
+        else if (mediaFinal == 6)
+            System.out.println("Recuperação");
+        
+        else
+            System.out.println("Aprovado");
+        
+    }
+    
+}
