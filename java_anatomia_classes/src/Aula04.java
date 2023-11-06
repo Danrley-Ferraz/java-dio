7cb57b0b32cc8add58bbafa5b14cc1cc49d407b8
Danrley Ferraz <110141527+Danrley-Ferraz@users.noreply.github.com>
2023-11-06 14:45:31 -0300

    Aprendendo

--- java_anatomia_classes/src/Aula04.java
+++ java_anatomia_classes/src/Aula04.java
@@ -1,3 +1,22 @@
 public class Aula04 {
-    
+
+    public static void main(String[] args) {
+
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+
+        // short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        // Para usar expressões que NUNCA mudam, a gente utiliza a palavra final e também a convenção de TODAS AS PALAVRAS DA EXPRESSÃO em caixa alta.
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+    }
+
 }
