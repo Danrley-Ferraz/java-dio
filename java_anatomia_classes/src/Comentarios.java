d16450f827c5897c91c42b779ca6961e8b95a19b
Danrley Ferraz <110141527+Danrley-Ferraz@users.noreply.github.com>
2023-11-09 01:39:31 -0300

    Aulas e exercicios em Java.

---
+++ java_anatomia_classes/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    
+    public static void main(String[] args){
+        
+        // Para fazer comentários de linha unica se utiliza o //
+
+        // ------------------------------------------------------------
+
+        /* Quando preciso utilizar multiplas linhas, se utiliza o /* 
+         * Ideal para detalhamento de projetos
+         */
+    }
+
+    /** Ao utilizar o /** é ideal que seja para documentação
+     *  É utilizada para identificar que você pretende elaborar um comentário
+     *  A nivel de documentação
+     */
+    
+    public void metodo(){
+
+    }
+}
